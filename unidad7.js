ad18fd51491136cd27d92944d68ddb322594e914
meinwissenschaft <47925280+meinwissenschaft@users.noreply.github.com>
2024-03-26 21:47:26 -0300

    Add files via upload

---
+++ unidad7.js
@@ -0,0 +1,7 @@
+var resp1 = prompt('¿Está usted familiarizado con los conceptos de filosofía Clasica?')
+var selector = prompt('Elija una sección: Introduccion, Exponentes o Posturas')
+var dificultad = prompt('Indique cuanto le cuesta entender filosofia (calificacion del 1 al 10): ')
+var calificacion1 = prompt('¿Cuántas horas le dedica estudiar filosofia? (1 al 10)')
+var calificacion2 = prompt('¿Cuántas horas le dedica leer filosofía?  (1 al 10)')
+
+var promedio = (parseInt(calificacion1) + parseInt(calificacion2))/2
